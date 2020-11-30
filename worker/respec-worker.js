9d845e70a4f3df1c287c01500b179377c752d994
Redmer Kronemeijer <kronemeijer@rdmr.eu>
2020-11-30 18:18:33 +0100

    add support for SPARQL and TURTLE syntax

--- worker/respec-worker.js
+++ worker/respec-worker.js
@@ -2,6 +2,10 @@
 "use strict";
 try {
   importScripts("https://www.w3.org/Tools/respec/respec-highlight");
+  importScripts("https://ghcdn.rawgit.org/redmer/highlightjs-turtle/master/dist/turtle.min.js");
+  importScripts("https://ghcdn.rawgit.org/redmer/highlightjs-sparql/master/dist/sparql.min.js");
+  // self.hljs.registerLanguage('turtle', self.hljsDefineTurtle);
+  // self.hljs.registerLanguage('turtle', self.hljsDefineSparql);
 } catch (err) {
   console.error("Network error loading highlighter", err);
 }
